8fb272f1ca28a161c8d63c7ad1ba41876b98ee91
Ke Jun <tkjun7559@gmail.com>
2024-03-21 03:17:21 +0800

    Chat and Header Logo

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css'; // Import CSS file for styling
+import logo from './picture/Logo.png'; // Import your logo image
 
 const Header = () => {
   return (
     <header className="pink-header">
-      <nav>
-        <ul>
-          <li><a href="/community">Community</a></li>
-          <li><a href="/blog">Blog Post</a></li>
-          <li><a href="/chat">Chat</a></li>
-        </ul>
-      </nav>
+      <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo" />
+        <div className="logoname">Career Journey Hub</div>
+      </div>
+      <div className="words">
+        <nav>
+          <ul>
+            <li><Link to="/community">Community</Link></li>
+            <li><Link to="/blog">Blog Post</Link></li>
+            <li><Link to="/chat">Chat</Link></li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
