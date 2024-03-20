a03975f493ab17d818f434ccd747e0e92a1416a6
kejun7219 <117891999+kejun7219@users.noreply.github.com>
2024-03-20 23:43:54 +0800

    edit header3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
+import Header from './Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <hi>hi</hi>
+    <div>
+      <Header/>
     </div>
   );
 }
