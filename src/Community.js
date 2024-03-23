ffc4191285b4734ec09e8b9e965632ce7b426bce
Ang Chin Zhen <angchinzhen@gmail.com>
2024-03-23 20:17:37 +0800

    community2

--- src/Community.js
+++ src/Community.js
@@ -1,7 +1,11 @@
 
 import React from "react";
 import "./CommunityPage.css"; // Import the CSS file
-import profilePicture from './picture/pro5.jpg'; ; // Import profile picture
+import friend1Image from './picture/pro1.jpeg'; 
+import friend2Image from './picture/pro2.jpeg'; 
+import friend3Image from './picture/pro3.jpg'; 
+import friend4Image from './picture/pro4.jpg'; 
+import friend5Image from './picture/pro5.jpg'; 
 
 const Community = () => {
     return (
@@ -12,7 +16,7 @@ const Community = () => {
             <div className="frame">
                 <div class="profile-container">
                      <div class="profile">
-                        <img src={profilePicture} alt="Profile" class="profile-picture" />
+                        <img src={friend5Image} alt="Profile" class="profile-picture" />
                      </div>
                 <div class="profileName">
                   <h2>John Doe</h2>
@@ -21,13 +25,24 @@ const Community = () => {
             </div>
                     <div className="content">
                         <div className="post">
-                            <p>This is a sample post. Feel free to interact with the community!</p>
+                            <p> for those who have pursued a CS degree, what advice would you give to someone like me who is considering alternative paths into the field? What are the pros and cons of self-learning versus formal education when it comes to landing a job in computer science?
+
+Any insights, personal experiences, or resources you could share would be immensely appreciated. Thank you in advance for your help! </p>
                             <div className="comments">
-                                <div className="comment">
-                                    <p>User 1: Nice post!</p>
-                                </div>
-                                <div className="comment">
-                                    <p>User 2: I agree!</p>
+                                <div classname="comment-user">
+                                    <div class="container">
+                                        <div class="comment-profile">
+                                            <img src={friend5Image} alt="Profile" class="profile-picture" />
+                                        </div>
+                                        <p>Jason</p>
+                                    </div>
+                                    <div className="comment">
+                                        <p>CS Degree: Pros - Solid theoretical foundation, widely recognized by employers. Cons - Time-consuming, expensive.</p>
+                                    </div>
+                                    <p>Muhd Ali</p>
+                                    <div className="comment">
+                                        <p>Self-learning: Pros - Flexible, cost-effective, focuses on practical skills. Cons - Lack of formal credentials may limit opportunities, requires strong self-discipline.</p>
+                                    </div>
                                 </div>
                                 {/* Add comment form */}
                                 <form className="add-comment-form">
