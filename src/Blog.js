f9a2ac6faaba57b817458789c04f07b1bf1af80d
Ke Jun <tkjun7559@gmail.com>
2024-03-24 01:00:20 +0800

    Blog commented

--- src/Blog.js
+++ src/Blog.js
@@ -1,31 +1,31 @@
 import React from 'react';
-import Post from './Post';
+// import Post from './Post';
 import './Blog.css';
 import friend5Image from './picture/pro1.jpeg'; 
 import picture from './picture/pro7.png';
 
-const initialPosts = [
-  {
-    id: 1,
-    user: 'John Doe',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    imageUrl: 'https://via.placeholder.com/150',
-    likes: 10,
-    comments: [
-      { id: 1, user: 'Jane Smith', comment: 'Nice post!' },
-      { id: 2, user: 'Alice', comment: 'Great!' }
-    ]
-  },
-  {
-    id: 2,
-    user: 'Alice',
-    content: 'Nulla facilisi. Ut fringilla velit vel nisi consequat.',
-    imageUrl: 'https://via.placeholder.com/150',
-    likes: 20,
-    comments: []
-  },
-  // Add more posts as needed
-];
+// const initialPosts = [
+//   {
+//     id: 1,
+//     user: 'John Doe',
+//     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+//     imageUrl: 'https://via.placeholder.com/150',
+//     likes: 10,
+//     comments: [
+//       { id: 1, user: 'Jane Smith', comment: 'Nice post!' },
+//       { id: 2, user: 'Alice', comment: 'Great!' }
+//     ]
+//   },
+//   {
+//     id: 2,
+//     user: 'Alice',
+//     content: 'Nulla facilisi. Ut fringilla velit vel nisi consequat.',
+//     imageUrl: 'https://via.placeholder.com/150',
+//     likes: 20,
+//     comments: []
+//   },
+//   // Add more posts as needed
+// ];
 
 const Blog = () => {
   return (
